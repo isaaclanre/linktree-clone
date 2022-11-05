7eea4207d1837816cba7b8b71d4eb25ef7268a22
Isaac Olanrewaju <isaacolanre@gmail.com>
2022-11-05 13:47:35 +0100

    Bug fixes: Fixed issues with the router

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import Footer from "./components/Footer";
 import Homepage from "./components/HomePage";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ContactUs from "./components/ContactUs";
 import Notfound from "./components/NotFound";
 
 const App = () => {
   return (
-    <HashRouter>
-      <>
+    <>
+      <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Homepage />}></Route>
+          <Route exact path="/*" element={<Homepage />}></Route>
           <Route exact path="/contact" element={<ContactUs />}></Route>
           <Route exact path="*" element={<Notfound />}></Route>
         </Routes>
-        <Footer />
-      </>
-    </HashRouter>
+      </BrowserRouter>
+      <Footer />
+    </>
   );
 };
 
