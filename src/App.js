0ce5e577641d7142dd52aaa3eb8247f664efec0b
Isaac Olanrewaju <isaacolanre@gmail.com>
2022-11-05 10:45:31 +0100

    feat: Added a not found page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Footer from "./components/Footer";
 import Homepage from "./components/HomePage";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ContactUs from "./components/ContactUs";
+import Notfound from "./components/NotFound";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Homepage />}></Route>
           <Route path="/contact" element={<ContactUs />}></Route>
+          <Route path="*" element={<Notfound />}></Route>
         </Routes>
         <Footer />
       </>
---
+++ src/components/NotFound.js
@@ -0,0 +1,12 @@
+import { Header, MainWrapper } from "./ContactUsStyle";
+
+const Notfound = () => {
+  return (
+    <MainWrapper>
+      {" "}
+      <Header> Sorry, this page does not exist! </Header>
+    </MainWrapper>
+  );
+};
+
+export default Notfound;
